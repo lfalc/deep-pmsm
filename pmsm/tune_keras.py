3c1d4b37dcfb71377f4180603722c108c0fb25d3
lfalc <lorenzfalcioni@gmail.com>
2024-06-11 10:29:24 +0200

    Add hyperparameter tuning using keras

---
+++ pmsm/tune_keras.py
@@ -0,0 +1,71 @@
+import keras_tuner
+from preprocessing.data import LightDataManager
+import preprocessing.config as cfg
+import numpy as np
+import tune_keras_class as tkc
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
+
+# Ensure reproducibility
+SEED = cfg.data_cfg["random_seed"]
+np.random.seed(SEED)
+
+dm = LightDataManager(cfg.data_cfg["file_path"])
+dm.featurize()
+
+# remove batch_size from dict, as it is not needed for build function
+batch_size = 32
+window_size = 32
+
+x_train = dm.tra_df[dm.x_cols + [dm.PROFILE_ID_COL]]
+y_train = dm.tra_df[dm.y_cols]
+x_val = dm.val_df[dm.x_cols + [dm.PROFILE_ID_COL]]
+y_val = dm.val_df[dm.y_cols]
+x_tst = dm.tst_df[dm.x_cols + [dm.PROFILE_ID_COL]]
+y_tst = dm.tst_df[dm.y_cols]
+
+callbacks = [
+    EarlyStopping(
+        monitor="val_loss",
+        min_delta=1e2,
+        patience=cfg.keras_cfg["early_stop_patience"],
+        verbose=1,
+    ),
+    ReduceLROnPlateau(
+        monitor="loss", patience=cfg.keras_cfg["early_stop_patience"] // 3
+    ),
+    ModelCheckpoint(
+        filepath='keras_tune/best_model.keras',
+        monitor='val_loss',
+        save_best_only=True,
+        save_weights_only=False,
+        verbose=1
+    ),
+    TensorBoard(log_dir='keras_tune/logs')
+]
+
+tuner = keras_tuner.RandomSearch(
+    hypermodel=tkc.PmsmHyperModel(),
+    objective='val_loss',
+    max_trials=2,
+    executions_per_trial=1,
+    overwrite=True,
+    directory='keras_tune',
+    project_name='cnn',)
+
+fit_args = {
+    "x": x_train,
+    "y": y_train,
+    "epochs": 200,
+    "validation_data": (x_val, y_val),
+    "batch_size": batch_size,
+    'callbacks': callbacks,
+    "p_id_col": dm.PROFILE_ID_COL,
+    "window_size": window_size,
+}
+
+tuner.search(**fit_args)
+
+# Get the top 2 models.
+models = tuner.get_best_models(num_models=2)
+best_model = models[0]
+best_model.summary()
---
+++ pmsm/tune_keras_class.py
@@ -0,0 +1,69 @@
+import keras_tuner
+import keras
+from preprocessing.cnn_model_utils import CNNKerasRegressor, build_cnn_model
+import pandas as pd
+
+
+class PmsmHyperModel(keras_tuner.HyperModel, CNNKerasRegressor):
+    def build(self, hp: keras_tuner.HyperParameters) -> keras.Model:
+        n_layers = hp.Int("n_layers", 1, 5)
+        n_units = hp.Int("n_units", 4, 6)
+        kernel_size = hp.Int("kernel_size", 2, 7)
+        regularization_rate = hp.Float(
+            "rSegularization_rate", 1e-9, 1e-1, sampling="log")
+        dropout_rate = hp.Float("dropout_rate", 0.2, 0.5)
+        lr_rate = hp.Float("lr_rate", 1e-7, 1e-2, sampling="log")
+
+        hyper_params = {
+            "x_shape": (32, 91),
+            "arch": "res",
+            "n_layers": n_layers,
+            "n_units": n_units,
+            "kernel_size": kernel_size,
+            "regularization_rate": regularization_rate,
+            "dropout_rate": dropout_rate,
+            "lr_rate": lr_rate,
+        }
+
+        model = build_cnn_model(**hyper_params)
+        return model
+
+    def fit(self, hp, model, x, y, callbacks=None, **kwargs):
+        assert isinstance(x, pd.DataFrame) and isinstance(
+            y, pd.DataFrame
+        ), f"{self.__class__.__name__} needs pandas DataFrames as input"
+
+        p_id_col = kwargs.pop("p_id_col", "p_id_col_not_found")
+        window_size = kwargs.pop("window_size", None)
+        data_cache = kwargs.pop("data_cache", {})
+        cache = data_cache.get("data_cache", None)
+        batch_size = kwargs.pop("batch_size", None)
+
+        if cache is not None:
+            # subsequent conduct iteration
+            seq_tra = cache["seq_tra"]
+            kwargs["validation_data"] = cache["seq_val"]
+        else:
+            # first conduct iteration
+            seq_tra = self._generate_batches(
+                x, y, p_id_col=p_id_col, batch_size=batch_size, window_size=window_size
+            )
+
+            x_val, y_val = kwargs.pop("validation_data")
+            seq_val = self._generate_batches(
+                x_val,
+                y_val,
+                p_id_col=p_id_col,
+                batch_size=batch_size,
+                window_size=window_size,
+            )
+
+            kwargs["validation_data"] = seq_val
+            new_cache = {"seq_tra": seq_tra, "seq_val": seq_val}
+            data_cache.update({"data_cache": new_cache})
+
+        fit_args = kwargs
+        fit_args['callbacks'] = callbacks
+        history = model.fit(seq_tra, **fit_args)
+
+        return history
